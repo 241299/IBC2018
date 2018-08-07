e8a01cb5624d95dab13f991ab2f8126acd6a371e
241299 <241299@users.noreply.github.com>
2018-08-07 18:20:55 +0300

    Replaced array sum with correct one

--- src/day_1/VectorSum.java
+++ src/day_1/VectorSum.java
@@ -1,18 +1,42 @@
+package day_1;
+
+import java.Math.*;
+
+/**
+	Demonstrates how to sum up two vectors of dimension N
+	Vectors are represented as arrays
+*/
+
 class VectorSum {
 	public static void main(String[] args) {
-		int n = 10, i;
-		int[] a = new int[n], c = new int[n];
-		int[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-		for (i = 0; i < n; i++) {
-			a[i] = (i + 1) * (i + 1);
+		
+		// Dimension of vectors (how many elements are in arrays)
+		final int N = 10;
+		
+		// Creating arrays
+		int[] a = new int[N], 
+			b = new int[N],
+			c = new int[N];
+			
+		// Filling arrays with random numbers
+		for (int i = 0; i < N; i++) {
+			a[i] = random(10, 1000);
+			b[i] = random(10, 1000);
+		}
+		
+		// Summing arrays to the third array
+		for (int i = 0; i < a.length; i++) {
 			c[i] = a[i] + b[i];
 		}
 
-		for (i = 0; i < n; i++) {
-			System.out.print(c[i]);
-			System.out.print(" ");
-		}
-		System.out.println();
+		System.out.println(Arrays.toString(c));
+	}
+	
+	/**
+		Gets random number between a and b
+	*/
+	
+	static int random (int a, int b) {
+		return (int) (Math.random() * (b - a)) + a;
 	}
 }
